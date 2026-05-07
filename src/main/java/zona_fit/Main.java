85840a90f153a909408038ccd6c2596e6b210360
Luisorozco42 <lorozcolazo@gmail.com>
2026-05-06 22:56:29 -0600

    making a connection (localhost credential doesnt matter)

--- src/main/java/zona_fit/Main.java
+++ src/main/java/zona_fit/Main.java
@@ -1,17 +1,13 @@
 package zona_fit;
 
+import zona_fit.conexion.Conexion;
+
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     static void main() {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        IO.println(String.format("Hello and welcome!"));
-
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            IO.println("i = " + i);
-        }
+        var conexion = Conexion.getConexion();
+        if (conexion != null)System.out.println("Conexion exitosa: " + conexion);
+        else System.out.println("Error al conectarse");
     }
 }
---
+++ src/main/java/zona_fit/conexion/Conexion.java
@@ -0,0 +1,22 @@
+package zona_fit.conexion;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+
+public class Conexion {
+    public static Connection getConexion(){
+        Connection conexion = null;
+        var baseDatos = "zona_fit_bd";
+        var url = "jdbc:mysql://localhost:3306/" + baseDatos;
+        var usuario = "root";
+        var password = "admin";
+
+        try {
+            Class.forName("com.mysql.cj.jdbc.Driver");
+            conexion = DriverManager.getConnection(url, usuario, password);
+        }catch (Exception e){
+            System.out.println("Error al conectar a la base de datos " + e.getMessage());
+        }
+        return conexion;
+    }
+}
